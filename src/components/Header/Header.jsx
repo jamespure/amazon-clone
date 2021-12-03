9c644a577ec734ea3b51e00cf82984f7668359f5
Seymour James <seymourpjames@gmail.com>
2021-12-03 00:38:16 -0700

    add home component and a few styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = () => {
   return (
@@ -10,19 +12,24 @@ const Header = () => {
       />
       <div className="header__search">
         <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <nav className="header__nav">
-        <div className="header__options">
+        <div className="header__option">
           <span className="header__optionLineOne">Hello,</span>
           <span className="header__optionLineTwo">Sign in</span>
         </div>
-        <div className="header__options">
+        <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
-        <div className="header__options">
+        <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionCart">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo cartCount">0</span>
         </div>
       </nav>
     </header>
